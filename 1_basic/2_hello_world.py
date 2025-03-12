e3adc9389d8cc20cd3576b2d81f3aaca83231cd1
Awanish rai <104734252+awanishroy@users.noreply.github.com>
2025-03-12 22:40:32 +0530

    learn_python

---
+++ 1_basic/2_hello_world.py
@@ -0,0 +1,28 @@
+from package.module import *
+
+""" Type 1 """
+
+# print("hello World !")
+
+
+""" Type 2 """
+
+# def hello_world():
+#     print("Hello World !")
+
+# hello_world()
+
+
+""" Type 3 """
+
+# class HelloWorld:
+
+#     def hello_world():
+#         print("Hello World !")
+
+# HelloWorld.hello_world()
+
+"""Type 4"""
+
+# print_hello_world()
+
---
+++ 2_data_type/1_data_type.py
@@ -0,0 +1,12 @@
+""" Data Type in Python 
+
+    1. Text Data Type (str)
+    2. Numeric Data Type (int, float, complex)
+    3. Sequence Data Type (list, tuple, range)
+    4. Mapping Data Type (dict)
+    5. Set Data Type (set, frozenset)
+    6. Bool Data Type (bool)
+    7. Binary Data Type (Byte, bytearray, memoryview)
+    8. None Data Type (None)
+
+"""
